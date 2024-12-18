8c226f547bdc6b7ae4073a549e879d757c91258a
Ilias Maaroufi <153554669+Iliasecc@users.noreply.github.com>
2024-12-18 23:58:49 +0100

    Update app.py

--- Tdlog/app.py
+++ Tdlog/app.py
@@ -14,9 +14,10 @@ openai.api_key = "sk-proj-KMzwctWqRGTtFsuq-_TgVW9y0Uu5npedy5ga7vXvIBvJ9zCe2WnsAU
 
 
 # Chemins vers les fichiers du modèle et du scaler
-model_path = "Projet_Tdlog_burn-out/Tdlog/model1.pkl"
-scaler_path = "Projet_Tdlog_burn-out/Tdlog/scaler1.pkl"
+model_path = "Tdlog/model1.pkl"
+scaler_path = "Tdlog/scaler1.pkl"
 data_file_path = "employees_data.json"
+grouped_data_file_path = "grouped_employees_data.json"
 
 # Charger le modèle
 with  open(model_path, "rb") as model_file:
